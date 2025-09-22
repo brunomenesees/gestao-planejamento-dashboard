7a862ca4ffcbc2d3f0dd56bab5340a99b22cf0fc
Bruno Meneses <70494415+brunomenesees@users.noreply.github.com>
2025-09-22 10:26:32 -0300

    Remove bcrypt password hashing code

--- generate-hash.js
+++ generate-hash.js
@@ -1,13 +1,1 @@
-import bcrypt from 'bcryptjs';
 
-// Coloque a senha que você quer usar aqui
-const password = ''; 
-const saltRounds = 10;
-
-bcrypt.hash(password, saltRounds, (err, hash) => {
-    if (err) {
-        throw err;
-    }
-    console.log(`Senha original: ${password}`);
-    console.log(`Hash gerado: ${hash}`);
-});
